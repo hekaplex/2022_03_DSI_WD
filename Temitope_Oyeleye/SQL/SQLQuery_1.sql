7f6552ef48480f9a14184bb259c520df16d8e1ee
tp4good <tp4good@gmail.com>
2022-03-31 18:52:34 -0400

    Imported Capstone Data

--- Temitope_Oyeleye/SQL/SQLQuery_1.sql
+++ Temitope_Oyeleye/SQL/SQLQuery_1.sql
@@ -1,4 +1,10 @@
-SELECT
-tOP 10
-FROM
-InvoiceArchive
+Select 'SELECT '+''''+TABLE_NAME +''''+ ' AS tbl_name, COUNT(*) FROM '+TABLE_NAME+' AS QTY UNION' from INFORMATION_SCHEMA.TABLES
+
+SELECT 'ContactUpdates' AS tbl_name, COUNT(*) FROM ContactUpdates AS QTY UNION
+SELECT 'GLAccounts' AS tbl_name, COUNT(*) FROM GLAccounts AS QTY UNION
+SELECT 'InvoiceArchive' AS tbl_name, COUNT(*) FROM InvoiceArchive AS QTY UNION
+SELECT 'InvoiceLineItems' AS tbl_name, COUNT(*) FROM InvoiceLineItems AS QTY UNION
+SELECT 'Invoices' AS tbl_name, COUNT(*) FROM Invoices AS QTY UNION
+SELECT 'Terms' AS tbl_name, COUNT(*) FROM Terms AS QTY UNION
+SELECT 'Vendors' AS tbl_name, COUNT(*) FROM Vendors AS QTY
+GO
