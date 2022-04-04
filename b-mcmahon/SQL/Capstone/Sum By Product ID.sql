4d425a2e6a75ae6e6ba639b6f622660832f8b4d8
b-mcmahon <bmcmahon.official@gmail.com>
2022-04-04 11:01:51 -0500

    correction

--- b-mcmahon/SQL/Capstone/Sum By Product ID.sql
+++ b-mcmahon/SQL/Capstone/Sum By Product ID.sql
@@ -1,8 +1,10 @@
-SELECT 
-	sum(Product_ID)
-	, Unit_quantity
-	, Weight
-FROM	
+SELECT
+	Product_ID
+	, SUM(Unit_quantity)
+	, SUM(Weight)
+FROM
 	OrderList
 GROUP BY 
 	Product_ID
+ORDER BY 
+	Product_ID ASC
